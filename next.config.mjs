2a4da095986c72759857e83262e277b05dccd0e3
MR-SHIV-SHARMA <shivps7568@gmail.com>
2025-03-16 21:50:34 +0530

    Refactor configuration files: migrate from next.config.js to next.config.mjs and enhance sitemap generation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: "/:path*",
+          headers: [
+            { key: "X-Robots-Tag", value: "index, follow" },
+            { key: "X-Frame-Options", value: "SAMEORIGIN" },
+            {
+              key: "Content-Security-Policy",
+              value:
+                "default-src 'self'; img-src *; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
+            },
+          ],
+        },
+      ];
+    },
+    experimental: {
+      metadataRoute: false, // ✅ Fix for sitemap issue
+    },
+  };
+  
+  export default nextConfig;
+  
--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -5,11 +5,13 @@ export async function GET() {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${pages
-        .map((page) => `<url><loc>https://www.shivshaktiss.in${page}</loc></url>`)
+        .map(
+          (page) => `<url><loc>https://www.shivshaktiss.in${page}</loc></url>`
+        )
         .join("")}
     </urlset>`;
 
-  return new NextResponse(sitemap, {
+  return new Response(sitemap, {
     headers: {
       "Content-Type": "application/xml",
     },
